f65c13858d09be4b21b46a6c3a21d33a5d757d89
SyKO6 <isxtrs06062008@gmail.com>
2025-10-07 16:54:13 -0500

    update

--- Syk0Shaders/mainShader.lua
+++ Syk0Shaders/mainShader.lua
@@ -23,7 +23,7 @@ end)
 -- ATMOSPHERE
 local atmosphere = Instance.new("Atmosphere")
 atmosphere.Name = "RealisticAtmosphere"
-atmosphere.Density = 0.5
+atmosphere.Density = 0.45
 atmosphere.Offset = 0.0
 atmosphere.Color = Color3.fromRGB(155, 180, 200)
 atmosphere.Parent = Lighting
@@ -31,16 +31,16 @@ atmosphere.Parent = Lighting
 -- COLOR CORRECTION
 local cc = Instance.new("ColorCorrectionEffect")
 cc.Name = "RealisticColorCorrection"
-cc.Brightness = 0.05
-cc.Contrast = 0.06
-cc.Saturation = -0.02
+cc.Brightness = 0.2
+cc.Contrast = 0.3
+cc.Saturation = 0.3
 cc.TintColor = Color3.fromRGB(255, 250, 240)
 cc.Parent = Lighting
 
 -- BLOOM
 local bloom = Instance.new("BloomEffect")
 bloom.Name = "RealisticBloom"
-bloom.Intensity = 1.0
+bloom.Intensity = 10.0
 bloom.Size = 24
 bloom.Threshold = 0.8
 bloom.Parent = Lighting
@@ -68,7 +68,7 @@ blur.Size = 4
 blur.Parent = Lighting
 
 -- SKY (si no hay uno existente)
-local hasSky = false
+local hasSky = false8
 for _, c in ipairs(Lighting:GetChildren()) do
 	if c:IsA("Sky") then
 		hasSky = true
