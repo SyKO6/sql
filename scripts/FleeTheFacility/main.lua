98ec287e8bfc675885e206db42c5b049ece0a13f
SyKO6 <isxtrs06062008@gmail.com>
2025-10-08 14:04:06 -0500

    update

--- scripts/FleeTheFacility/main.lua
+++ scripts/FleeTheFacility/main.lua
@@ -1,3 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SyKO6/sql/refs/heads/main/scripts/FleeTheFacility/intro.lua"))()
+
 --// CONFIGURACIONES INICIALES
 local Players = game:GetService("Players")
 local Lighting = game:GetService("Lighting")
